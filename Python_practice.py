2cf1f1da81a5c2191ec8db2471db6834ffe49470
Laura Hemydin <laura.hemydin@gmail.com>
2022-09-09 14:17:22 -0500

    Adding the election analysis.

--- Python_practice.py
+++ Python_practice.py
@@ -1,201 +1,102 @@
-print("Hello World")
-print(type(3))
-print(type(2019))
-ballots = 1341
-print(type(ballots))
-print(type(73.81))
-print(type('Hello World'))
-print(type(True))
+# print("Hello World")
 
-print("------------")
-print(5 + 2 * 3)
-print(8 // 5 - 3)
-print(8 + 22 * 2 - 4)
-print(16- 3 / 2 + 7 - 1)
-print(3 ** 3 % 5)
-print(5 + 9 * 3 / 2 - 4)
-print("------------")
-print((5 + 2) * 3)
-print((8 // 5) - 3)
-print(8 + (22 * (2 - 4)))
-print(16 - 3 / (2 + 7) - 1)
-print(3 ** (3 % 5))
-print(5 + (9 * 3 / (2 - 4)))
-
-counties = ["Arapahoe","Denver","Jefferson"]
-print(counties)
-print(counties[0])
-print(counties[1])
-print(counties[2])
-print(counties[-1])
-print(counties[-2])
-print(counties[-3])
-
-print("--------------")
-print(len(counties))
-print("--------------")
-print(counties[0:2])
-print(counties[0:1])
-print(counties[:2])
-print(counties[1:])
-print(counties[1:3])
-print("--------------")
-
-counties.append("El Paso")
-counties.insert(2, "El Paso")
-print(len(counties))
-print(counties)
-
-counties.remove("El Paso")
-print(len(counties))
-print(counties)
-
-counties.pop(3)
-#counties.pop(-1)
-print(counties)
-
-print("-------Update-------")
-counties[2] = "El Paso"
-print(counties)
-print("----------------------")
-
-counties = ["Arapahoe","Denver","Jefferson"]
-counties.insert(1,"El Paso")
-counties.remove("Arapahoe")
-print(counties)
-counties.remove("Denver")
-counties.insert(2,"Denver")
-counties.append("Arapahoe")
-print(counties)
-print("----------------")
-
-
-counties_tuple = ("Arapahoe","Denver","Jefferson")
-print(len(counties_tuple))
-print(counties_tuple[1])
-print(counties_tuple[:2])
-print(counties_tuple[:-1])
-print("------------------")
-
-counties_dict = {}
-counties_dict["Arapahoe"]=422829
-counties_dict["Denver"]=463353
-counties_dict["Jefferson"] = 432438
-len(counties_dict)
-
-print(counties_dict["Arapahoe"])
-print(counties_dict["Denver"])
-print(counties_dict["Jefferson"])
-print(len(counties_dict))
-print(counties_dict.items())
-print(counties_dict.keys())
-print(counties_dict.values())
-print(counties_dict.get("Denver"))
-print("---------------")
-
-voting_data = []
-voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
-voting_data.append({"county":"Denver", "registered_voters": 463353})
-voting_data.append({"county":"Jefferson", "registered_voters": 432438})
-len(voting_data)
-
-print(voting_data[0]["county"])
-print(voting_data[0].get("county"))
-print(voting_data[0]["registered_voters"])
-print(voting_data[0].get("registered_voters"))
-voting_data.append({"county": "El Paso", "registered_voters":461149})
-voting_data.remove({"county":"Arapahoe", "registered_voters": 422829})
-voting_data.append({"county":"Denver", "registered_voters": 463353})
-voting_data.append({"county":"Jefferson", "registered_voters": 432438})
-voting_data.pop(0)
-voting_data.pop(0)
-voting_data.append({"county":"Arapahoe", "registered_voters": 422829})
-print(voting_data)
-print("------------------")
-
-# # How many votes did you get?
-# my_votes=int(input("How many votes did you get in the election?"))
-# # Total votes in the election
-# total_votes=int(input("What is the total votes in the election?"))
-# # Calculate the percentage of votes you received.
-# percentage_votes=(my_votes / total_votes) * 100
-# print("I received" + str(percentage_votes)+"% of the total votes.")
-# print("-"* 100)
 # counties = ["Arapahoe", "Denver", "Jefferson"]
-# if counties[1] == 'Denver':
-#     print(counties[1])
-# print("--------------")
+# if counties[1] == "Denver":
+#         print(counties[1])
 
-temperature = int(input("What is the temperature outside? "))
+# # temperature = int(input("What is the temperature outside?"))
+# # if temperature >80:
+# #     print("Turn on the AC.")
+# # else: 
+# #     print("Open the windows.")
 
-if temperature > 80:
-    print("Turn on the AC.")
-else:
-   print("Open the windows.")
+# # grade = int(input("What is your Grade?  "))
+# # if grade >= 90:
+# #     print("Your grade is an A")
+# # else:
+# #     if grade >= 80:
+# #         print("Your grade is a B")
+# #     else:
+# #         if grade >= 70:
+# #             print("Your grade is a C")
+# #         else:
+# #             if grade >= 60:
+# #              print("Your grade is a D")
+# #             else:
+# #                 print("Your grade is an an F.")
 
-# print("--------------")
+# # counties = ["Arapahoe","Denver","Jefferson"]
+# # if "El Paso" in counties:
+# #     print("El Paso is in the list of counties.")
+# # else:
+# #     print("El Paso is not the list of counties.")
 
-# What is the score?
-score=int(input("What is your test score? "))
+# # if "Arapahoe" in counties and "El Paso" in counties:
+# #     print("Arapahoe and El Paso are in the list of counties.")
+# # else:
+# #     print("Arapahoe or El Paso is not in the list of counties.")
 
-# Determinte the grade.
-if score>=90: 
-    print('Your grade is an A.')
-else:
-    if score>=80:
-        print('Your grade is a B.')
-    else:
-        if score >= 70:
-            print ('Your grade is a C.')
-        else:
-            if score >=60:
-                print('Your grade is a D')
-            else:
-                print('Your grade is an F.')
+# # if "Arapahoe" in counties or "El Paso" in counties:
+# #     print("Arapahoe or El Paso is in the list of counties.")
+# # else:
+# #     print("Arapahoe and El Paso are not in the list of counties.")
 
-# What is the score?
-#score=int(input("What is your test score? "))
+# x = 0
+# while x <=5:
+#     print(x)
+#     x = x+1
 
-# Determinte the grade.
-#if score>=90: 
-    #print('Your grade is an A.')
-    #elif score >= 80:
-        #print('Your grade is a B.')
-    #elif score >= 70:
-        #print ('Your grade is a C.')
-    #elif score >=60:
-        #print('Your grade is a D.')
-    #else:
-        #print('Your grade is an F.')
+# for county in counties:
+#     print(county)
 
-# print("--------------")
+counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# for county in counties_dict:
+#     print(county)
 
-counties = ["Arapahoe","Denver","Jefferson"]
-if "El Paso" in counties:
-    print("El Paso is in the list of counties.")
-else:
-    print("El Paso is not the list of counties.")
+# for county in counties_dict.keys():
+#     print(county)
 
-if "Arapahoe" in counties and "El Paso" in counties:
-    print("Arapahoe and El Paso are in the list of counties.")
-else:
-    print("Arapahoe or El Paso is not in the list of counties.")
+# for voters in counties_dict.values():
+#     print(voters)
 
-if "Arapahoe" in counties and "El Paso" not in counties:
-    print("Only Arapahoe is in the list of counties.")
-else:
-    print("Arapahoe is in the list of counties and El Paso is not in the list of counties.")
+# for county in counties_dict:
+#     print (counties_dict[county])
 
-count=7
-while count < 1:
-    print("Hello World")
+# for county in counties_dict:
+#     print(counties_dict.get(county))   
 
-counties_tuple=("Arapahoe","Denver","Jefferson")
-for i in range(len(counties_tuple)): 
-    print(counties_tuple[i])
-    print("------------------")
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829},
+                {"county":"Denver", "registered_voters": 463353},
+                {"county":"Jefferson", "registered_voters": 432438}]
 
-x=0
-while x<=5:
-    print(x)
-    x=x+1
+# for county_dict in voting_data:
+#     print(county_dict)
+
+# for county_dict in voting_data:
+#     for value in county_dict.values():
+#         print(value)
+
+# for county_dict in voting_data:
+#     print(county_dict.values())
+
+# for county_dict in voting_data:
+#     for value in county_dict:
+#         print(value)
+
+# for county_dict in voting_data:
+#     print(county_dict['registered_voters'])
+
+# for county_dict in voting_data:
+#     print(county_dict['county'])
+
+# Skill Drill #1 (3.2.11)
+# counties_dict = {"Arapahoe": 422829, "Denver": 463353, "Jefferson": 432438}
+# for county, voters in counties_dict.items():
+#     print(f"{county} county has {voters:{2},} registered voters.")
+
+# Skill Drill #2 (3.2.11)
+voting_data = [{"county":"Arapahoe", "registered_voters": 422829}, 
+                {"county":"Denver", "registered_voters": 463353}, 
+                {"county":"Jefferson", "registered_voters": 432438}]
+for county_dict in voting_data:
+        print(f"{county_dict['county']} county has {county_dict['registered_voters']:{2},} registered voters.")
